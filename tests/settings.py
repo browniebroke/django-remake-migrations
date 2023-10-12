cf5218f73e2247912ccca34b2637adff91dcd59c
Bruno Alla <alla.brunoo@gmail.com>
2023-10-12 15:58:01 +0100

    test: scaffold a test app

---
+++ tests/settings.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
+
+SECRET_KEY = "NOTASECRET"  # noqa S105
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+        "ATOMIC_REQUESTS": True,
+    },
+}
+
+TIME_ZONE = "UTC"
+
+INSTALLED_APPS = [
+    "tests.testapp",
+    "remake_migrations",
+    # Force django_migrations creation by having an app with migrations:
+    "django.contrib.contenttypes",
+]
+
+USE_TZ = True
