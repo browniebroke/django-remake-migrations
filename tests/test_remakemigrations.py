12d5a70659e3f0a93c0b76dcdafb46425a529dcf
Bruno Alla <alla.brunoo@gmail.com>
2023-10-12 22:09:17 +0100

    test: write test for step 2 and 3

--- tests/test_remakemigrations.py
+++ tests/test_remakemigrations.py
@@ -1,7 +1,10 @@
 from __future__ import annotations
 
+import json
+import os
 import sys
 import time
+from datetime import datetime
 from pathlib import Path
 from textwrap import dedent
 
@@ -56,3 +59,61 @@ class RemakeMigrationsTests(TestCase):
 
         graph_json = Path(__file__).parent / "graph.json"
         assert graph_json.exists()
+
+    def test_success_step_2_and_3(self):
+        (self.migrations_dir / "__init__.py").touch()
+        graph_json = Path(__file__).parent / "graph.json"
+        graph_json.write_text(
+            json.dumps(
+                {
+                    "testapp": [
+                        ["testapp", "0003_other_thing"],
+                        ["testapp", "0002_something"],
+                        ["testapp", "0001_initial"],
+                    ]
+                }
+            )
+        )
+
+        out, err, returncode = run_command("remakemigrations", "--step", "2")
+
+        assert out == ""
+        assert err == ""
+        assert returncode == 0
+
+        dir_files = os.listdir(self.migrations_dir)
+        assert dir_files == ["__init__.py", "0001_initial.py"]
+
+        initial_0001 = self.migrations_dir / "0001_initial.py"
+        content = initial_0001.read_text()
+
+        assert "from django.db import migrations, models" in content
+        assert "class Migration(migrations.Migration):" in content
+        assert "initial = True" in content
+        assert "replaces = " not in content
+
+        out, err, returncode = run_command("remakemigrations", "--step", "3")
+
+        assert out == ""
+        assert err == ""
+        assert returncode == 0
+
+        dir_files = os.listdir(self.migrations_dir)
+        today = datetime.today()
+        initial_remade_name = f"0001_remaked_{today:%Y%m%d}_initial.py"
+        assert dir_files == [
+            "__init__.py",
+            initial_remade_name,
+        ]
+
+        initial_remade = self.migrations_dir / initial_remade_name
+        content = initial_remade.read_text()
+
+        assert "from django.db import migrations, models" in content
+        assert "class Migration(migrations.Migration):" in content
+        assert "initial = True" in content
+        assert (
+            "replaces = [['testapp', '0001_initial'], "
+            "['testapp', '0002_something'], "
+            "['testapp', '0003_other_thing']]" in content
+        )
