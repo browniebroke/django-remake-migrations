46612dd77ef96a0ebfe55d634171f25a7fd8a534
Bruno Alla <alla.brunoo@gmail.com>
2023-11-14 21:01:41 +0000

    chore: rename package to django_remake_migrations
    BREAKING CHANGE: the main package has been renamed from remake_migrations to django_remake_migrations. You'll need to change it in `INSTALLED_APPS`.

--- src/django_remake_migrations/apps.py
+++ src/django_remake_migrations/apps.py
@@ -5,5 +5,5 @@ from django.utils.translation import gettext_lazy as _
 class RemakeMigrationAppConfig(AppConfig):
     """App config for the remake migration app."""
 
-    name = "remake_migrations"
+    name = "django_remake_migrations"
     verbose_name = _("remake migrations")
