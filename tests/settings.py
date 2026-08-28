5a2b3edd8282a2e07f705500f2bb9825805474b4
Bruno Alla <browniebroke@users.noreply.github.com>
2026-08-28 17:37:26 +0200

    test: run tests against PostgreSQL (#841)

--- tests/settings.py
+++ tests/settings.py
@@ -1,11 +1,17 @@
 from __future__ import annotations
 
+import os
+
 SECRET_KEY = "NOTASECRET"  # noqa S105
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": ":memory:",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_DB", "postgres"),
+        "USER": os.environ.get("POSTGRES_USER", "postgres"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "postgres"),
+        "HOST": os.environ.get("POSTGRES_HOST", "localhost"),
+        "PORT": os.environ.get("POSTGRES_PORT", "5432"),
         "ATOMIC_REQUESTS": True,
     },
 }
